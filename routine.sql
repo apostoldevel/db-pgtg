c5dd170803f9aead623b10bf5839512dc57b0487
Преподобный Ален <alienufo@inbox.ru>
2022-11-14 02:23:45 +0300

    Committing updates.

--- routine.sql
+++ routine.sql
@@ -3,18 +3,26 @@
 --------------------------------------------------------------------------------
 
 CREATE OR REPLACE FUNCTION tg.send_message (
-  bot_id    uuid,
-  chat_id   int,
-  text      text
-) RETURNS   uuid
+  bot_id        uuid,
+  chat_id       int,
+  text          text,
+  parse_mode    text DEFAULT null
+) RETURNS       uuid
 AS $$
 DECLARE
-  v_token   text;
+  v_token       text;
+  content       jsonb;
 BEGIN
   SELECT token INTO v_token FROM bot.list WHERE id = bot_id;
 
   IF FOUND THEN
-    RETURN http.create_request(format('https://api.telegram.org/bot%s/sendMessage', v_token), 'POST', null, json_build_object('chat_id', chat_id, 'text', text)::text);
+    content := jsonb_build_object('chat_id', chat_id, 'text', text);
+
+    IF parse_mode IS NOT NULL THEN
+	  content := content || jsonb_build_object('parse_mode', parse_mode);
+	END IF;
+
+    RETURN http.create_request(format('https://api.telegram.org/bot%s/sendMessage', v_token), 'POST', null, content::text);
   END IF;
 
   RETURN null;
