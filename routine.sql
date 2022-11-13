3d5d0fb9dda00efcf1d5b012cd3c515a83179f6c
Преподобный Ален <alienufo@inbox.ru>
2022-11-13 17:26:04 +0300

    Added Heartbeat function.

--- routine.sql
+++ routine.sql
@@ -12,9 +12,12 @@ DECLARE
   v_token   text;
 BEGIN
   SELECT token INTO v_token FROM bot.list WHERE id = bot_id;
+
   IF FOUND THEN
     RETURN http.create_request(format('https://api.telegram.org/bot%s/sendMessage', v_token), 'POST', null, json_build_object('chat_id', chat_id, 'text', text)::text);
   END IF;
+
+  RETURN null;
 END;
 $$ LANGUAGE plpgsql
   SECURITY DEFINER
